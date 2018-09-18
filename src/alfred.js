70e864e6c9a7b2b786c29a1b85540552c706cd54
Amila Welihinda <amilajack@gmail.com>
2018-09-18 11:02:37 -0700

    Add update-notifier

--- src/alfred.js
+++ src/alfred.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 // @flow
 const program = require('commander');
+const updateNotifier = require('update-notifier');
+const pkg = require('../package.json');
+
+updateNotifier({ pkg }).notify();
 
 program
   .version('0.0.1')
