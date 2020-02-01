2604f4c663e1bf9b9712b33bcee19ce354c115e4
Amila Welihinda <amilajack@gmail.com>
2020-02-01 15:02:55 -0800

    remove serial execution of skills

--- examples/hello-world/jest.config.js
+++ examples/hello-world/jest.config.js
@@ -1,5 +1,7 @@
-{
-  "transform": {
-    "^.+\\.jsx?$": "./node_modules/jest-transformer.js"
-  }
-}
+module.exports = {
+          transform: {
+            '^.+.jsx?$': '/Users/amila/Documents/Projects/alfred/examples/hello-world/node_modules/jest-transformer.js'
+          },
+          rootDir: '/Users/amila/Documents/Projects/alfred/examples/hello-world'
+        };
+        
